834f1ff3397f335bb750bfb49567a27f606a934a
Bryan Rochadel <xadrez90@outlook.com>
2023-10-02 00:52:22 -0300

    manaIcons

--- src/js/mtgScryFall.js
+++ src/js/mtgScryFall.js
@@ -1,11 +1,57 @@
 // Mapeamento de símbolos de mana para URLs das imagens SVG
 const manaIcons = {
-    "{W}": "/src/svg/WhiteManaColor.svg",
-    "{U}": "/src/svg/BlueManaColor.svg",
-    "{B}": "/src/svg/BlackManaColor.svg",
+    "{W}": "https://svgs.scryfall.io/card-symbols/W.svg",
+    "{U}": "https://svgs.scryfall.io/card-symbols/U.svg",
+    "{B}": "https://svgs.scryfall.io/card-symbols/B.svg",
     "{R}": "https://svgs.scryfall.io/card-symbols/R.svg",
     "{G}": "https://svgs.scryfall.io/card-symbols/G.svg",
-    "{T}": "/src/svg/Tap.svg",
+    "{∞}": "https://svgs.scryfall.io/card-symbols/INFINITY.svg",
+    "{W/U}": "https://svgs.scryfall.io/card-symbols/WU.svg",
+    "{W/B}": "https://svgs.scryfall.io/card-symbols/WB.svg",
+    "{B/R}": "https://svgs.scryfall.io/card-symbols/BR.svg",
+    "{B/G}": "https://svgs.scryfall.io/card-symbols/BG.svg",
+    "{U/B}": "https://svgs.scryfall.io/card-symbols/UB.svg",
+    "{U/R}": "https://svgs.scryfall.io/card-symbols/UR.svg",
+    "{R/G}": "https://svgs.scryfall.io/card-symbols/RG.svg",
+    "{R/W}": "https://svgs.scryfall.io/card-symbols/RW.svg",
+    "{G/W}": "https://svgs.scryfall.io/card-symbols/GW.svg",
+    "{G/U}": "https://svgs.scryfall.io/card-symbols/GU.svg",
+    "{B/G/P}": "https://svgs.scryfall.io/card-symbols/BGP.svg",
+    "{B/R/P}": "https://svgs.scryfall.io/card-symbols/BRP.svg",
+    "{G/U/P}": "https://svgs.scryfall.io/card-symbols/GUP.svg",
+    "{G/W/P}": "https://svgs.scryfall.io/card-symbols/GWP.svg",
+    "{R/G/P}": "https://svgs.scryfall.io/card-symbols/RGP.svg",
+    "{R/W/P}": "https://svgs.scryfall.io/card-symbols/RWP.svg",
+    "{U/B/P}": "https://svgs.scryfall.io/card-symbols/UBP.svg",
+    "{U/R/P}": "https://svgs.scryfall.io/card-symbols/URP.svg",
+    "{W/B/P}": "https://svgs.scryfall.io/card-symbols/WBP.svg",
+    "{W/U/P}": "https://svgs.scryfall.io/card-symbols/WUP.svg",
+    "{2/W}": "https://svgs.scryfall.io/card-symbols/2W.svg",
+    "{2/U}": "https://svgs.scryfall.io/card-symbols/2U.svg",
+    "{2/B}": "https://svgs.scryfall.io/card-symbols/2B.svg",
+    "{2/R}": "https://svgs.scryfall.io/card-symbols/2R.svg",
+    "{2/G}": "https://svgs.scryfall.io/card-symbols/2G.svg",
+    "{P}": "https://svgs.scryfall.io/card-symbols/P.svg",
+    "{W/P}": "https://svgs.scryfall.io/card-symbols/WP.svg",
+    "{U/P}": "https://svgs.scryfall.io/card-symbols/UP.svg",
+    "{B/P}": "https://svgs.scryfall.io/card-symbols/BP.svg",
+    "{R/P}": "https://svgs.scryfall.io/card-symbols/RP.svg",
+    "{G/P}": "https://svgs.scryfall.io/card-symbols/GP.svg",
+    "{HW}": "https://svgs.scryfall.io/card-symbols/HW.svg",
+    "{HR}": "https://svgs.scryfall.io/card-symbols/HR.svg",
+    "{T}": "https://svgs.scryfall.io/card-symbols/T.svg",
+    "{Q}": "https://svgs.scryfall.io/card-symbols/Q.svg",
+    "{X}": "https://svgs.scryfall.io/card-symbols/X.svg",
+    "{Y}": "https://svgs.scryfall.io/card-symbols/Y.svg",
+    "{Z}": "https://svgs.scryfall.io/card-symbols/Z.svg",
+    "{S}": "https://svgs.scryfall.io/card-symbols/S.svg",
+    "{C}": "https://svgs.scryfall.io/card-symbols/C.svg",
+    "{CHAOS}": "https://svgs.scryfall.io/card-symbols/CHAOS.svg",
+    "{E}": "https://svgs.scryfall.io/card-symbols/E.svg",
+    "{TK}": "https://svgs.scryfall.io/card-symbols/TK.svg",
+    "{PW}": "https://svgs.scryfall.io/card-symbols/PW.svg",
+    "{½}": "https://svgs.scryfall.io/card-symbols/HALF.svg",    
+    "{A}": "https://svgs.scryfall.io/card-symbols/A.svg",
 };
 
 const PesquisaInput = document.querySelector("[name=card-name]");
@@ -97,26 +143,24 @@ function createCardElement(imageUrl, altText) {
 function mapManaSymbolsToIcons(manaCost) {
     const regex = /{[^{}]+}/g;
     const matches = manaCost.match(regex);
-
+  
     if (matches) {
-        matches.forEach((match) => {
-            const symbol = match.trim(); // Remova os caracteres de chaves extras
-            const iconUrl = manaIcons[symbol];
-            if (iconUrl) {
-                // Crie um elemento de imagem com o URL da imagem de mana correspondente
-                const manaIcon = document.createElement("img");
-                manaIcon.src = iconUrl;
-                manaIcon.alt = "não encontrado";      
-                manaIcon.className = "mana-icon";
-
-                // Substitua o símbolo de mana pelo elemento de imagem
-                manaCost = manaCost.replace(new RegExp(symbol, 'g'), manaIcon.outerHTML);
-            }
-        });
+      matches.forEach((match) => {
+        const symbol = match.trim();
+        const iconUrl = manaIcons[symbol];
+        if (iconUrl) {
+          const manaIcon = document.createElement("img");
+          manaIcon.src = iconUrl;
+          manaIcon.alt = "";
+          manaIcon.className = "mana-icon";
+  
+          manaCost = manaCost.replace(new RegExp(symbol, 'g'), manaIcon.outerHTML);
+        }
+      });
     }
-
+  
     return manaCost;
-}
+  }
 
 function showModal(cardData) {
     const modalCardName = document.getElementById("modalCardName");
